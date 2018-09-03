c568be7375e73f37ae650b56ed2023a245233576
Alexis Serrano <serrano.321@hotmail.com>
2018-09-03 00:12:28 -0500

    calendario vue

---
+++ src/components/Calendario.vue
@@ -0,0 +1,46 @@
+<template>
+	<div>
+		<calendar :eventArray="this.eventos" />
+	</div>
+</template>
+
+<script>
+	import {
+		Calendar
+	} from 'quasar-calendar'
+	export default {
+		components: {
+			'calendar': Calendar
+		},
+		data() {
+			return {
+				eventos: [{
+						id: 1,
+						summary: 'Cita para acuerdo reparatorio',
+						description: 'deberan preentarse en la uat',
+						location: 'uat fiscalia xalapa',
+						start: {
+							dateTime: '2018-09-16T14:00:00', // ISO 8601 formatted
+						},
+						end: {
+							dateTime: '2018-09-16T16:30:00',
+						},
+						color: 'positive',
+						attendees: [{
+							id: 1,
+							email: 'fiscaliaveracruz@correo.com',
+							displayName: 'John Q. Public',
+							organizer: false,
+							self: false,
+							resource: false
+						}]
+					}
+				]
+			}
+		},
+	}
+</script>
+
+<style scoped>
+	
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import './styles/quasar.styl'
+import lang from 'quasar-framework/i18n/es'
+import 'quasar-extras/material-icons'
+import Quasar from 'quasar'
+
+Vue.use(Quasar, {
+  config: {}, i18n: lang
+ })
+
 Vue.config.productionTip = false
 
 new Vue({
